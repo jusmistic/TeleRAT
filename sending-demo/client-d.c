12298b6aa2742281d81202099a10381364df9676
Dumpkung <poppular105@gmail.com>
2018-03-26 21:53:45 +0700

    add comments and refactor some code

--- sending-demo/client-d.c
+++ sending-demo/client-d.c
@@ -14,6 +14,7 @@ int main(int argc , char *argv[])
     char message[2000], server_reply[2000];
      
     //Create socket
+    //If cannot create socket it return value of socket_desc valuable = -1
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if (socket_desc == -1)
     {
@@ -25,6 +26,7 @@ int main(int argc , char *argv[])
     server.sin_port = htons(8888);
  
     //Connect to remote server
+    //If cannot connect to server it return value of (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0 and end program
     if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
         perror("connect error");
@@ -32,26 +34,31 @@ int main(int argc , char *argv[])
     }
      
     printf("Connected\n");
-    //Send some data
+    //Send some data (client --> server)
     while(1){
  
-        //Receive a reply from the server
+        //Receive a reply from the server(Server have massage to client and client read this message)
+	//bzero(message,2000) --> set a byte string (message)
         bzero(message,2000);
+        //If read(socket_desc, message , 2000 ) < 0 it means Server didn't send anything to client
         if( read(socket_desc, message , 2000 ) < 0)
         {
-            printf("recv failed");
+            printf("recieve failed");
         }printf("%s\n",message);
         
         //Send to server
-        // bzero(message,2000);
+        //After Client has recieve form server, Client write message back to server
         printf("--- Sending init --- \n");
         int i=0;
         char tmp[250],buf[250];
+        //use popen in order to pointer to the first memory location that hold the results (results ---> value of message)
         FILE *fp = popen(message, "r");
         {
+            //fgets use to read next input line to keep in charactor array with value MAXLINE - 1
             while (fgets(buf, 200, fp))
             {
             printf("%s\n:",buf);
+            //If write(socket_desc , buf , strlen(buf)) < 0 it means client didn't send anything to server
             if( write(socket_desc , buf , strlen(buf)) < 0)
             {
                 printf("Send failed");
@@ -63,6 +70,7 @@ int main(int argc , char *argv[])
             }
         }
         //Stop sending
+        //After Client send message to server
         printf("Stop sent..\n");
         usleep(1000000); //1sec
         sprintf(message,"%c",OP_STOP);
