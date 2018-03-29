3f8b4a3b5ee81d0418cf6c51e98b178db22747fa
jusmistic <jusmistic@gmail.com>
2018-03-29 11:15:52 +0700

    Refactor Function.

--- sending-demo/move.c
+++ sending-demo/move.c
@@ -10,22 +10,25 @@ struct paths{
     char cmd[5000];
 };
 
+struct paths getPath(struct paths pathGet){
+    if (getcwd(pathGet.nowpath, sizeof(pathGet.nowpath)) == NULL)
+       perror("getcwd() error");
+    len = strlen(pathGet.pName);
+    //parse ./move -> /move
+    strncpy(pathGet.pName,pathGet.pName+1,len-1);
+    //add null byte for stop string
+    pathGet.pName[len-1] ='\0';
+
+    return pathGet;
+}
 struct paths move(struct paths pathMove){
     int len=0;
     //Get now program location
-    if (getcwd(pathMove.nowpath, sizeof(pathMove.nowpath)) == NULL)
-       perror("getcwd() error");
-    len = strlen(pathMove.pName);
-    //parse ./move -> /move
-    strncpy(pathMove.pName,pathMove.pName+1,len-1);
-    //add null byte for stop string
-    pathMove.pName[len-1] ='\0';
-
+    pathMove = getPath(pathMove);
     //parse path to cmd
     sprintf(pathMove.cmd,"mv %s%s %s%s",pathMove.nowpath,pathMove.pName,pathMove.moveTo,pathMove.pName);
     return pathMove;
 }
-
 
 int main(int argc , char *argv[]){
     char buf[250];
