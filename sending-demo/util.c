be777a70f0972068fbe1041962a05f16f40e2277
jusmistic <jusmistic@gmail.com>
2018-04-10 15:58:02 +0700

    add I AM ROOOOT function

--- sending-demo/util.c
+++ sending-demo/util.c
@@ -9,6 +9,12 @@ struct cmdinfo{
     char moveTo[2048];
 };
 
+void i_am_root(){
+    if(getuid() != 0){
+        printf("\nI'm not ROOOOOOT!.\n(Please Run with Sudo or ROOT)\n\n");
+        exit(1);
+    }
+}
 
 //get Program Name
 void getpName(char *pName){
@@ -93,9 +99,10 @@ int main(int argc , char *argv[]){
 
     struct cmdinfo cmd;
     strcpy(cmd.pName,argv[0]);
-    strcpy(cmd.moveTo,argv[1]);
+    // strcpy(cmd.moveTo,argv[1]);
 
 
+    i_am_root();
     // move(cmd.moveTo);
     // path = boom(cmd.pName);
     // serviceSetting(); 
